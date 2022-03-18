61c077651e4d2570ceea89eaf094eeaf87605fd5
Geraldo Vedrossi Neto <73123156+geraldovedrossi@users.noreply.github.com>
2022-03-18 17:36:34 -0300

    Subindo JavaScript pt2

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,20 @@ function pressionar() {
     alert("Oloco, você clicou mesmo?!")
 }
 
+function validacao(id){
+    let input = document.getElementById(id)
+    let txt = document.querySelector('#valida'+id)
+
+        if (input.value.length < 3) {
+            txt.innerHTML = id + " inválido!"
+            txt.style.color = 'red'
+        } else {
+            txt.innerHTML = id + " válido!"
+            txt.style.color = 'green'
+        }
+
+}
+
 function verificarCampos() {
     if(nome.value == "" || email.value == "" || assunto.value == "") {
         alert("Todos os campos devem ser preenchidos!")
@@ -17,3 +31,6 @@ function verificarCampos() {
         alert("Sua menssagem foi enviada!")
     }
 }
+
+nome.style.width=assunto.width
+email.style.width=assunto.width
